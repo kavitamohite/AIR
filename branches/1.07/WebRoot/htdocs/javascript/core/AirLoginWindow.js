4c5be56f3d47fa49d180a61833e7c27f712ceffe
ercva <ercva@d2170373-ae6f-44b7-9150-2eec3990abfc>
2012-08-20 15:01:32 +0000

    git-svn-id: http://10.205.116.236/svn/AIR@19 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/1.07/WebRoot/htdocs/javascript/core/AirLoginWindow.js
+++ branches/1.07/WebRoot/htdocs/javascript/core/AirLoginWindow.js
@@ -129,11 +129,6 @@ AIR.AirLoginWindow = Ext.extend(Ext.Window, {
 		
 		var cwid = this.getComponent('pAirLoginWindow').getComponent('tfCwid').getValue();
 		var password = this.getComponent('pAirLoginWindow').getComponent('tfPassword').getValue();
-
-		if(cwid.length === 0)
-			cwid = 'ercva';
-		if(password.length === 0)
-			password = 'creati95';
 		
 		this.fireEvent('login', cwid, password);
 	},
