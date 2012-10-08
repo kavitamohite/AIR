6e799a329b037927a8533c6ee0593ce02a4f5422
evafl <evafl@d2170373-ae6f-44b7-9150-2eec3990abfc>
2012-10-08 09:42:32 +0000

    fix ldap auth warning
    git-svn-id: http://10.205.116.236/svn/AIR@102 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/1.09/src/com/bayerbbs/applrepos/service/LDAPAuthWSDelegate.java
+++ branches/1.09/src/com/bayerbbs/applrepos/service/LDAPAuthWSDelegate.java
@@ -1,18 +1,5 @@
 package com.bayerbbs.applrepos.service;
 
-import java.util.List;
-import java.util.Random;
-import net.sf.ehcache.Cache;
-import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.Element;
-import com.bayerbbs.applrepos.common.LDAPAuthCeye;
-import com.bayerbbs.applrepos.constants.ApplreposConstants;
-import com.bayerbbs.applrepos.domain.AppRepAuthData;
-import com.bayerbbs.applrepos.dto.PersonsDTO;
-import com.bayerbbs.applrepos.dto.RolePersonDTO;
-import com.bayerbbs.applrepos.hibernate.ApplReposHbn;
-import com.bayerbbs.applrepos.hibernate.ItSecUserHbn;
-import com.bayerbbs.applrepos.hibernate.PersonsHbn;
 
 @javax.jws.WebService(targetNamespace = "http://service.applrepos.bayerbbs.com/", serviceName = "LDAPAuthWSService", portName = "LDAPAuthWSPort")
 public class LDAPAuthWSDelegate {
@@ -34,7 +21,7 @@ public class LDAPAuthWSDelegate {
 	}
 
 	public boolean isLoginValid(String username, String token) {
-		return lDAPAuthWS.isLoginValid(username, token);
+		return LDAPAuthWS.isLoginValid(username, token);
 	}
 
 	public void logout(String username, String token) {
