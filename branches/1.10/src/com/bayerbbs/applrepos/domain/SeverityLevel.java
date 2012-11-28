7b5d15e789efd25cd37e72d65b27515aee31ad63
ercva <ercva@d2170373-ae6f-44b7-9150-2eec3990abfc>
2012-11-28 09:42:08 +0000

    bugfix: einloggen war nicht möglich
    git-svn-id: http://10.205.116.236/svn/AIR@201 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/1.10/src/com/bayerbbs/applrepos/domain/SeverityLevel.java
+++ branches/1.10/src/com/bayerbbs/applrepos/domain/SeverityLevel.java
@@ -2,10 +2,12 @@ package com.bayerbbs.applrepos.domain;
 
 import java.io.Serializable;
 
-import org.hibernate.annotations.Entity;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Entity;
 
 
 @Entity
@@ -13,10 +15,16 @@ import javax.persistence.Table;
 @SequenceGenerator(name = "MySeqSeverityLevel", sequenceName = "TBADM.SEQ_SEVERITY_LEVEL")
 
 public class SeverityLevel implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 168789052260094674L;
 
+
+	
+	@Id @GeneratedValue
+	public Long getID() {
+		return id;
+	}
+	public void setID(Long id) {
+		this.id = id;
+	}
+	protected Long id;
 }
