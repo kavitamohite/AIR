abe71105cb4a2e9830d950f0aeb8de962bb9166c
exbf <exbf@d2170373-ae6f-44b7-9150-2eec3990abfc>
2012-12-14 12:34:30 +0000

    each role only once
    git-svn-id: http://10.205.116.236/svn/AIR@224 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/1.10/src/com/bayerbbs/applrepos/hibernate/ApplReposHbn.java
+++ branches/1.10/src/com/bayerbbs/applrepos/hibernate/ApplReposHbn.java
@@ -22,15 +22,15 @@ import com.bayerbbs.applrepos.dto.RolePersonDTO;
 
 public class ApplReposHbn {
 
-	private static final String SELECT_ROLES = "SELECT   ROL.Role_Id, " +
+	private static final String SELECT_ROLES = "SELECT DISTINCT ROL.Role_Id, " +
 					         "ROL.Role_Name " +
 					         "FROM     ROLE ROL  " +
 					         "INNER JOIN ROLE_PERSON R2P ON ROL.Role_Id=R2P.Role_Id AND Current_Date BETWEEN R2P.Date_Start AND R2P.Date_End AND R2P.Del_Timestamp IS NULL " + 
 					         "INNER JOIN ROLE_INTERFACE R2I ON R2P.Role_Id=R2I.Role_Id AND R2I.Del_Timestamp IS NULL  " +
 					         "INNER JOIN INTERFACES INT ON R2I.Interface_Id=INT.Interfaces_Id AND INT.Del_Timestamp IS NULL AND INT.Token = 'AIR' " +
 								"WHERE    R2P.Cwid = :Cwid " +
-								"AND      ROL.Del_Timestamp IS NULL";
-	private static final String SELECT_ROLE_BUSINESS_ESSENTIAL = "SELECT   ROL.Role_Id, " +
+								"AND      ROL.Del_Quelle IS NULL";
+	private static final String SELECT_ROLE_BUSINESS_ESSENTIAL = "SELECT DISTINCT ROL.Role_Id, " +
 															    "ROL.Role_Name " +
 															    "FROM     ROLE ROL  " +
 															    "INNER JOIN ROLE_PERSON R2P ON ROL.Role_Id=R2P.Role_Id AND Current_Date BETWEEN R2P.Date_Start AND R2P.Date_End AND R2P.Del_Timestamp IS NULL " + 
@@ -38,7 +38,7 @@ public class ApplReposHbn {
 															    "INNER JOIN INTERFACES INT ON R2I.Interface_Id=INT.Interfaces_Id AND INT.Del_Timestamp IS NULL AND INT.Token = 'AIR' " +
 																"WHERE    R2P.Cwid = :Cwid " +
 																"AND      ROL.Role_Name = :Role_Name "+
-																"AND      ROL.Del_Timestamp IS NULL"; 
+																"AND      ROL.Del_Quelle IS NULL"; 
 
 	private static final String TRANSBASE_ORA_20000 = "ORA-20000: ";
 	/** The logger. */
