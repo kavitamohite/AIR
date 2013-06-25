d65597f79a13859010879dc6e71bb49a7ba23bd1
exbf <exbf@d2170373-ae6f-44b7-9150-2eec3990abfc>
2013-06-25 15:30:39 +0000

    gutted
    git-svn-id: http://10.205.116.236/svn/AIR@410 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.00/src/com/bayerbbs/applrepos/service/CiGroupsWS.java
+++ branches/2.00/src/com/bayerbbs/applrepos/service/CiGroupsWS.java
@@ -1,12 +1,12 @@
 package com.bayerbbs.applrepos.service;
 
 import com.bayerbbs.applrepos.dto.CiGroupsDTO;
-import com.bayerbbs.applrepos.hibernate.CiGroupsHbn;
 
 public class CiGroupsWS {
 
 	public CiGroupsDTO[] getGroupsList(Long ciId, Long groupTypeId) {
-		return CiGroupsHbn.getArrayFromList(CiGroupsHbn.findCiGroupsBy(ciId, groupTypeId));
+//		return CiGroupsHbn.getArrayFromList(CiGroupsHbn.findCiGroupsBy(ciId, groupTypeId));
+		return null;
 	}
 	
 }
