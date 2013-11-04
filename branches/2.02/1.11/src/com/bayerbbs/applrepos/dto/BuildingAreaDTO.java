8f71de4b4b723cf4a10e981d2f6a51d623de6bac
evbsi <evbsi@d2170373-ae6f-44b7-9150-2eec3990abfc>
2013-11-04 14:00:33 +0000

    git-svn-id: http://10.205.116.236/svn/AIR@504 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.02/1.11/src/com/bayerbbs/applrepos/dto/BuildingAreaDTO.java
+++
@@ -1,8 +0,0 @@
-package com.bayerbbs.applrepos.dto;
-
-import java.io.Serializable;
-
-public class BuildingAreaDTO extends LocationDTO implements Serializable {
-	private static final long serialVersionUID = -8683668255902232558L;
-	
-}
