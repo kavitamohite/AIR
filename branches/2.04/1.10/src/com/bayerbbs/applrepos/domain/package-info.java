c7891e2c59fae5fbd0e82ca649fbe290083fef3d
evbsi <evbsi@d2170373-ae6f-44b7-9150-2eec3990abfc>
2014-06-12 09:21:57 +0000

    something wrong
    git-svn-id: http://10.205.116.236/svn/AIR@550 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.04/1.10/src/com/bayerbbs/applrepos/domain/package-info.java
+++
@@ -1,5 +0,0 @@
-@TypeDefs( { @TypeDef(name = "onezero-boolean", typeClass = OneZeroBoolean.class) })
-package com.bayerbbs.applrepos.domain;
-
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
