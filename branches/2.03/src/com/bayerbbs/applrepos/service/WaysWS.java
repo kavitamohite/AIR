03a628a043f5a10200a77fd01eaf83eb974e17f9
eokeg <eokeg@d2170373-ae6f-44b7-9150-2eec3990abfc>
2015-04-27 13:28:37 +0000

    Added by vandana 10793
    git-svn-id: http://10.205.116.236/svn/AIR@587 d2170373-ae6f-44b7-9150-2eec3990abfc

---
+++ branches/2.03/src/com/bayerbbs/applrepos/service/WaysWS.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package com.bayerbbs.applrepos.service;
+
+import com.bayerbbs.applrepos.constants.AirKonstanten;
+import com.bayerbbs.applrepos.domain.Ways;
+import com.bayerbbs.applrepos.dto.PathwayDTO;
+import com.bayerbbs.applrepos.hibernate.PathwayHbn;
+
+/**
+ * @author eokeg
+ *
+ */
+public class WaysWS {
+	
+	
+	public CiEntityEditParameterOutput createWays(BaseEditParameterInput input) {
+		
+		CiEntityEditParameterOutput output = new CiEntityEditParameterOutput();
+		if(null != input &&(LDAPAuthWS.isLoginValid(input.getCwid(), input.getToken())) ){
+			PathwayDTO pathwayDTO = getPathwayDTOFromFromEditInput(input);
+			output = PathwayHbn.createPathway(input.getCwid(), pathwayDTO, true);
+			if(AirKonstanten.RESULT_OK.equals(output.getResult())){
+				Ways way = PathwayHbn.findByName(input.getName());
+				output.setCiId(way.getId());
+			}
+			
+		}
+		return output;
+	}
+	
+	protected PathwayDTO getPathwayDTOFromFromEditInput(BaseEditParameterInput input){
+		PathwayDTO pathwayDTO = new PathwayDTO();
+		//pathwayDTO.setTableId(AirKonstanten.TABLE_ID_SITE);//commented by vandana
+		pathwayDTO.setTableId(AirKonstanten.TABLE_ID_WAYS);
+		//Specifics
+		pathwayDTO.setId(input.getId());
+		pathwayDTO.setName(input.getName());
+
+
+		
+		//Contacts
+		pathwayDTO.setCiOwner(input.getCiOwner());
+		pathwayDTO.setCiOwnerHidden(input.getCiOwnerHidden());
+		pathwayDTO.setCiOwnerDelegate(input.getCiOwnerDelegate());
+		pathwayDTO.setCiOwnerDelegateHidden(input.getCiOwnerDelegateHidden());
+
+		//Agreements
+		pathwayDTO.setSlaId(input.getSlaId());
+		pathwayDTO.setServiceContractId(input.getServiceContractId());
+
+		
+		//Protection
+		pathwayDTO.setItSecSbAvailabilityId(input.getItSecSbAvailabilityId());
+		pathwayDTO.setItSecSbAvailabilityTxt(input.getItSecSbAvailabilityTxt());//setItSecSbAvailabilityDescription/getItSecSbAvailabilityDescription
+//		pathwayDTO.setClassInformationId(input.getClassInformationId());
+//		pathwayDTO.setClassInformationExplanation(input.getClassInformationExplanation());
+		pathwayDTO.setItSecSbIntegrityId(input.getItSecSbIntegrityId());
+		pathwayDTO.setItSecSbIntegrityTxt(input.getItSecSbIntegrityTxt());
+		pathwayDTO.setItSecSbConfidentialityId(input.getItSecSbConfidentialityId());
+		pathwayDTO.setItSecSbConfidentialityTxt(input.getItSecSbConfidentialityTxt());
+		
+		//Compliance
+		pathwayDTO.setItset(input.getItset());
+		pathwayDTO.setTemplate(input.getTemplate());
+		pathwayDTO.setItsecGroupId(input.getItSecGroupId());
+		//im BaseEditParameterInput heisst es s/getItSecGroupId, in CiBaseDTO heisst es s/getItsecGroupId 
+		//(s vs S !!) Ursprung ApplicationWS.getApplicationDTOFromEditInput() REFAC!!
+		pathwayDTO.setRefId(input.getRefId());
+		
+		pathwayDTO.setRelevanceGR1435(input.getRelevanceGR1435());
+		pathwayDTO.setRelevanceGR1920(input.getRelevanceGR1920());
+//		pathwayDTO.setRelevanceICS(input.getRelevanceICS());
+//		pathwayDTO.setRelevanzItsec(input.getRelevanzITSEC());
+		pathwayDTO.setGxpFlag(input.getGxpFlag());
+		pathwayDTO.setGxpFlagId(input.getGxpFlag());
+		
+		pathwayDTO.setGpsccontactSupportGroupHidden(input.getGpsccontactSupportGroupHidden());
+		pathwayDTO.setGpsccontactChangeTeamHidden(input.getGpsccontactChangeTeamHidden());
+		pathwayDTO.setGpsccontactServiceCoordinatorHidden(input.getGpsccontactServiceCoordinatorHidden());
+		pathwayDTO.setGpsccontactEscalationHidden(input.getGpsccontactEscalationHidden());
+		pathwayDTO.setGpsccontactCiOwnerHidden(input.getGpsccontactCiOwnerHidden());
+		pathwayDTO.setGpsccontactServiceCoordinatorIndivHidden(input.getGpsccontactServiceCoordinatorIndivHidden());
+		pathwayDTO.setGpsccontactEscalationIndivHidden(input.getGpsccontactEscalationIndivHidden());
+		pathwayDTO.setGpsccontactResponsibleAtCustomerSideHidden(input.getGpsccontactResponsibleAtCustomerSideHidden());
+		pathwayDTO.setGpsccontactSystemResponsibleHidden(input.getGpsccontactSystemResponsibleHidden());
+		pathwayDTO.setGpsccontactImpactedBusinessHidden(input.getGpsccontactImpactedBusinessHidden()); 
+
+		pathwayDTO.setGpsccontactSupportGroup(input.getGpsccontactSupportGroup());
+		pathwayDTO.setGpsccontactChangeTeam(input.getGpsccontactChangeTeam());
+		pathwayDTO.setGpsccontactServiceCoordinator(input.getGpsccontactServiceCoordinator());
+		pathwayDTO.setGpsccontactEscalation(input.getGpsccontactEscalation());
+		pathwayDTO.setGpsccontactCiOwner(input.getGpsccontactCiOwner());
+		pathwayDTO.setGpsccontactServiceCoordinatorIndiv(input.getGpsccontactServiceCoordinatorIndiv());
+		pathwayDTO.setGpsccontactEscalationIndiv(input.getGpsccontactEscalationIndiv());
+		pathwayDTO.setGpsccontactResponsibleAtCustomerSide(input.getGpsccontactResponsibleAtCustomerSide());
+		pathwayDTO.setGpsccontactSystemResponsible(input.getGpsccontactSystemResponsible());
+		pathwayDTO.setGpsccontactImpactedBusiness(input.getGpsccontactImpactedBusiness());
+		
+		pathwayDTO.setDownStreamAdd(input.getDownStreamAdd());
+		pathwayDTO.setDownStreamDelete(input.getDownStreamDelete());
+		return pathwayDTO;
+		
+	}
+
+}
