d16639cd1a8a866715d61c167057913479a90a9a
eokeg <eokeg@d2170373-ae6f-44b7-9150-2eec3990abfc>
2015-05-06 13:53:30 +0000

    added by vandana 11315
    git-svn-id: http://10.205.116.236/svn/AIR@608 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.03/src/com/bayerbbs/applrepos/dto/BuildingDTO.java
+++ branches/2.03/src/com/bayerbbs/applrepos/dto/BuildingDTO.java
@@ -13,6 +13,47 @@ public class BuildingDTO extends LocationDTO implements Serializable {
 	private String postalCode;
 	private String location;
 	
+	
+	//Added by vandana
+	private String Provider_Name;
+	private String Provider_NameHidden;
+	private String Provider_Address;
+	private String Provider_AddressHidden;
+	
+	public String getProvider_Name() {
+		return Provider_Name;
+	}
+
+	public void setProvider_Name(String Provider_Name) {
+		this.Provider_Name = Provider_Name;
+	}
+
+	public String getProvider_NameHidden() {
+		return Provider_NameHidden;
+	}
+
+	public void setProvider_NameHidden(String Provider_NameHidden) {
+		this.Provider_NameHidden = Provider_NameHidden;
+	}
+	
+	public String getProvider_Address() {
+		return Provider_Address;
+	}
+
+	public void setProvider_Address(String Provider_Address) {
+		this.Provider_Address = Provider_Address;
+	}
+
+	public String getProvider_AddressHidden() {
+		return Provider_AddressHidden;
+	}
+
+	public void setProvider_AddressHidden(String Provider_AddressHidden) {
+		this.Provider_AddressHidden = Provider_AddressHidden;
+	}
+	//ended by vandana
+
+	
 
 	public String getAlias() {
 		return alias;
