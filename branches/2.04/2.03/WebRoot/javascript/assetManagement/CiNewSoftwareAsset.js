3f3b135ec84c40e53628cc4b182a07dcb691be0c
eovwj <eovwj@d2170373-ae6f-44b7-9150-2eec3990abfc>
2015-07-01 11:36:00 +0000

    RFC--10562
    git-svn-id: http://10.205.116.236/svn/AIR@759 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.04/2.03/WebRoot/javascript/assetManagement/CiNewSoftwareAsset.js
+++ branches/2.04/2.03/WebRoot/javascript/assetManagement/CiNewSoftwareAsset.js
@@ -77,8 +77,13 @@ AIR.CiNewSoftwareAsset = Ext.extend(AIR.AirView, {
                         xtype: 'AIR.CiBusiness',
                         itemId: 'businessInformation'
                     }]
-                }]
-            }, {
+                }],
+            },{
+            	itemId: 'assetHistoryView',
+				xtype: 'AIR.CiHistoryView',
+				hidden: true
+			},
+			{
                 xtype: 'panel',
                 itemId: 'buttonPanel',
                 layout: 'column',
@@ -90,6 +95,7 @@ AIR.CiNewSoftwareAsset = Ext.extend(AIR.AirView, {
                     xtype: 'button',
                     itemId: 'saveBtn',
                     text: 'Save',
+                    hidden: true,
                     style: {
                         fontSize: 12,
                         margin: '8 10 0 0',
@@ -102,6 +108,7 @@ AIR.CiNewSoftwareAsset = Ext.extend(AIR.AirView, {
                     xtype: 'button',
                     itemId: 'cancelBtn',
                     text: 'Cancel',
+                    hidden: true,
                     style: {
                         fontSize: 12,
                         margin: '8 10 0 0',
@@ -117,17 +124,7 @@ AIR.CiNewSoftwareAsset = Ext.extend(AIR.AirView, {
                         margin: '8 10 0 0',
                         width: 80
                     }
-                }, {
-                    xtype: 'button',
-                    itemId: 'savecloseBtn',
-                    text: 'Save & Close',
-                    style: {
-                        //marginBottom : 10,
-                        fontSize: 12,
-                        margin: '8 10 0 0',
-                        width: 80
-                    }
-                }, {
+                },  {
                     xtype: 'button',
                     itemId: 'bReset',
                     text: 'Reset all Entries',
@@ -138,36 +135,21 @@ AIR.CiNewSoftwareAsset = Ext.extend(AIR.AirView, {
                         width: 80
                     }
                 }, {
-                    xtype: 'button',
-                    itemId: 'masterBtn',
-                    text: 'Request for new master data',
-                    style: {
-                        //marginBottom : 10,
-                        fontSize: 14,
-                        margin: '8 10 0 0',
-                        width: 80
-                    }
-                }, {
-                    xtype: 'button',
-                    itemId: 'changemasterBtn',
-                    text: 'Change master data',
-                    style: {
-                        //marginBottom : 10,
-                        fontSize: 12,
-                        margin: '8 10 0 0',
-                        width: 80
-                    }
-                }, {
-                    xtype: 'button',
-                    itemId: 'assetCheck',
-                    text: 'Asset Checked',
-                    style: {
-                        fontSize: 12,
-                        margin: '8 10 0 0',
-                        width: 80
-
-                    }
-                }]
+					xtype : 'button',
+					itemId : 'bHistory',
+					text : 'Asset History',
+					listeners: {
+                        click: {
+                            fn: this.onAssetHistoryButton,
+                            scope: this
+                        }
+					},
+					style : {
+						fontSize : 14,
+						margin : '8 10 0 0',
+						width:80
+					}
+				}  ]
             }]
         });
 
@@ -175,8 +157,213 @@ AIR.CiNewSoftwareAsset = Ext.extend(AIR.AirView, {
 
         var bReset = this.getComponent('buttonPanel').getComponent('bReset');
         bReset.on('click', this.resetFormFields, this);
+    	var cbManufacturer = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product').getComponent('cbManufacturer');
+        cbManufacturer.on('select', this.onFieldKeyUp, this);
+      
+      var cbSubCategory = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product').getComponent('cbSubCategory');
+         cbSubCategory.on('select', this.onFieldKeyUp, this);
+      
+      var cbType = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product').getComponent('cbType');
+         cbType.on('select', this.onFieldKeyUp, this);
+
+      var cbModel = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product').getComponent('pmodel').getComponent('cbModel');
+         cbModel.on('select', this.onFieldKeyUp, this);
+      
+      var cbCostcenter = this.getComponent('bottomPanel').getComponent('rightPanel').getComponent('businessInformation').getComponent('pCost').getComponent('cbCostcenter');
+         cbCostcenter.on('select', this.onFieldKeyUp, this);
+         
+       var cbSapAsset = this.getComponent('bottomPanel').getComponent('rightPanel').getComponent('businessInformation').getComponent('cbSapAsset');
+       cbSapAsset.on('select', this.onFieldKeyUp, this);
+       
+      var tfRequester = this.getComponent('bottomPanel').getComponent('rightPanel').getComponent('businessInformation').getComponent('pRequester').getComponent('tfRequester');
+      tfRequester.on('select', this.onFieldKeyUp, this);
+      
+      var cbRoom = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('cbRoom');
+      cbRoom.on('select', this.onFieldKeyUp, this);
+      
+      var cbBuilding =this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('cbBuilding');
+      cbBuilding.on('select', this.onFieldKeyUp, this);
+      
+      var cbSite = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('cbSite');
+      cbSite.on('select', this.onFieldKeyUp, this);
+      
+      var cbCountry = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('cbCountry');
+      cbCountry.on('select', this.onFieldKeyUp, this);
+      
+      var cbRack = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('pRackposition').getComponent('cbRack');
+      cbRack.on('select', this.onFieldKeyUp, this);
 
     },
+    onAssetHistoryButton: function(){
+    	var assetId = this.getComponent('topPanel').getComponent('assetId').getValue();
+    	console.log(assetId);
+    	
+    	var loadMask = Util.createMask('Loading', Ext.get('historyListView'));
+		loadMask.show();
+		var historyListStore = AIR.AirStoreFactory.createHistoryListStore();
+
+		var params = {
+			cwid: AAM.getCwid(),
+			token: AAM.getToken(),
+			id:  30914,//18452,//AIR.AirApplicationManager.getAssetId(),///assetId,//
+			tableId: 20//AAM.getTableId()
+		};
+		
+		historyListStore.addListener('load', function() {
+			loadMask.hide();
+			assetHistoryWindow = new Ext.Window({
+	            title: 'History',
+	            layout: 'fit',
+	            //autoScroll: true,
+	            width: 1230,
+	            height: 600,
+	            modal: true,
+	            closeAction: 'hide',
+	            items: [{
+	          		    	xtype: 'grid',
+	        		        id: 'historyListView',
+	        		        layout: 'fit',
+	        		        height: 400,
+	        		    	store: this,
+	        		        emptyText: 'No data',
+	        		        border: false,
+	        		        //autoScroll: true,
+	        		        
+	        		        columns: [{
+	        		            header: 'Date Time',
+	        		            dataIndex: 'datetime',
+	        		            id: 'historyDatetime',
+	        					menuDisabled: true,
+	        					width: 150
+	        		        },{
+	        		            header: 'Change Source',
+	        		            dataIndex: 'changeSource',
+	        		            id: 'historyChangeSource',
+	        					menuDisabled: true,
+	        					width: 150
+	        		        },{
+	        		            header: 'Change User',
+	        		            dataIndex: 'changeDBUser',
+	        		           	id: 'historyChangeDBUser',
+	        					menuDisabled: true,
+	        					width: 150
+	        		        },{
+	        		            header: 'Change user CWID',
+	        		            dataIndex: 'changeUserCWID',
+	        		            id: 'historyChangeUserCWID',
+	        					menuDisabled: true,
+	        					width: 150
+	        		    	},{
+	        		            header: 'Attribute Name',
+	        		            dataIndex: 'changeAttributeName',
+	        		            id: 'historyChangeAttributeName',
+	        					menuDisabled: true,
+	        					width: 150
+	        		        },{
+	        		            header: 'Asset id',
+	        		            dataIndex: 'ciId',
+	        		            id: 'ciId',
+	        					menuDisabled: true,
+	        					width: 150
+	        		    	},{
+	        		    		header: 'Old value',
+	        		            dataIndex: 'changeAttributeOldValue',
+	        		            id: 'historyChangeAttributeOldValue',
+	        					menuDisabled: true,
+	        					width: 150
+	        		    	},{
+	        		            header: 'New value',
+	        		            dataIndex: 'changeAttributeNewValue',
+	        		            id: 'historyChangeAttributeNewValue',
+	        					menuDisabled: true,
+	        					width: 150
+	        		        },{
+	        		        	header: 'Info Type',
+	        		        	dataIndex: 'infoType',
+	        		        	id: 'infoType',
+	        					hidden: true,
+	        					menuDisabled: true
+	        		        }],
+	        				
+	        				view: new Ext.grid.GroupingView({})
+	        		    }]
+	    	});
+	    	assetHistoryWindow.show();
+		});
+		
+		historyListStore.load({
+			params: params
+		});
+    	
+    	
+    },
+    
+    resetFormFields: function(assetData){
+    	
+    	var saveBtn = this.getComponent('buttonPanel').getComponent('saveBtn');
+		var cancelBtn = this.getComponent('buttonPanel').getComponent('cancelBtn');
+    	
+    	var topPanel = this.getComponent('topPanel');
+    	topPanel.update(assetData);
+
+    	var product = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product');
+    	product.update(assetData);
+    	
+    	var location = this.getComponent('bottomPanel').getComponent('rightPanel').getComponent('location');
+    	location.update(assetData);
+    	
+    	var business = this.getComponent('bottomPanel').getComponent('rightPanel').getComponent('businessInformation');
+    	business.update(assetData);
+    	
+    	var contact = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('contacts');
+    	contact.update(assetData);
+    	
+    	var technics = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('technics');
+    	technics.update(assetData);
+    	
+    	saveBtn.hide();
+		cancelBtn.hide();
+        
+    },
+    
+    onFieldKeyUp: function(textfield, event) {
+		this.onCiAssetChange();
+	},
+	
+	onCiAssetChange:function() {		
+		this.enableAssetButtons();
+		this.ciModified = true;
+	},
+	
+	enableAssetButtons: function() {	
+		var saveBtn = this.getComponent('buttonPanel').getComponent('saveBtn');
+		var cancelBtn = this.getComponent('buttonPanel').getComponent('cancelBtn');
+		var cbManufacturerValue = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product').getComponent('cbManufacturer').getRawValue();
+		var cbSubCategoryValue=this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product').getComponent('cbSubCategory').getRawValue();
+		var cbTypeValue = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product').getComponent('cbType').getRawValue();
+		var cbModelValue = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('product').getComponent('pmodel').getComponent('cbModel').getRawValue();
+		var cbRoomValue = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('cbRoom').getRawValue();
+		var cbBuildingValue =this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('cbBuilding').getRawValue();
+		var cbSiteValue = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('cbSite').getRawValue();
+		var cbCountryValue = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('cbCountry').getRawValue();
+		var cbRackValue = this.getComponent('bottomPanel').getComponent('leftPanel').getComponent('location').getComponent('pRackposition').getComponent('cbRack').getRawValue();
+		var cbCostcenterValue = this.getComponent('bottomPanel').getComponent('rightPanel').getComponent('businessInformation').getComponent('pCost').getComponent('cbCostcenter').getRawValue();
+		var cbSapAssetValue = this.getComponent('bottomPanel').getComponent('rightPanel').getComponent('businessInformation').getComponent('cbSapAsset').getRawValue();
+		var tfRequesterValue = this.getComponent('bottomPanel').getComponent('rightPanel').getComponent('businessInformation').getComponent('pRequester').getComponent('tfRequester').getRawValue();
+		
+			if(cbManufacturerValue.length>0 && cbSubCategoryValue.length>0 && cbTypeValue.length>0 && cbModelValue .length>0
+				&& cbCountryValue.length>0 && cbSiteValue.length>0 && cbBuildingValue.length>0 && cbRoomValue.length && cbRackValue.length>0 
+				/*&& cbCostcenterValue.length>0*/ && cbSapAssetValue.length>0 /*&&tfRequesterValue.length>0*/ ){
+			saveBtn.show();
+			cancelBtn.show();
+			
+			this.fireEvent('airAction', this, 'clear');
+		} else {
+			saveBtn.hide();
+			cancelBtn.hide();
+		}
+       
+	},
 
 //    updateLabels: function(labels) {
 //    	AIR.getStoreByName('manufactureListStore');
