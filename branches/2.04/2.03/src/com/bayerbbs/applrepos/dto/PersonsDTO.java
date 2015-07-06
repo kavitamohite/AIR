5e707ed4a71bf1f3eb8951c9fcc4e0000cd40658
eovwj <eovwj@d2170373-ae6f-44b7-9150-2eec3990abfc>
2015-07-06 11:29:56 +0000

    Asset Management
    git-svn-id: http://10.205.116.236/svn/AIR@788 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.04/2.03/src/com/bayerbbs/applrepos/dto/PersonsDTO.java
+++ branches/2.04/2.03/src/com/bayerbbs/applrepos/dto/PersonsDTO.java
@@ -8,57 +8,71 @@ public class PersonsDTO {
 	 * @author evafl
 	 *
 	 */
-	
+
 	private Long personId;
 	private String cwid;
 	private String persNr;
 	private String lastname;
 	private String firstname;
 	private String mail;
-	
+	private String orgUnit;
+
 	public Long getPersonId() {
 		return personId;
 	}
+
 	public void setPersonId(Long personId) {
 		this.personId = personId;
 	}
+
 	public String getCwid() {
 		return cwid;
 	}
+
 	public void setCwid(String cwid) {
 		this.cwid = cwid;
 	}
+
 	public String getPersNr() {
 		return persNr;
 	}
+
 	public void setPersNr(String persNr) {
 		this.persNr = persNr;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getMail() {
 		return mail;
 	}
+
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
-	/**
-	 * returns the full name with cwid for display usage
-	 * 
-	 * Hasenclever, Juergen (HAS)
-	 * 
-	 * @return
-	 */
+
+	public String getOrgUnit() {
+		return orgUnit;
+	}
+
+	public void setOrgUnit(String orgUnit) {
+		this.orgUnit = orgUnit;
+	}
+
 	public String getDisplayNameFull() {
 		StringBuffer sb = new StringBuffer();
 
@@ -66,7 +80,6 @@ public class PersonsDTO {
 			sb.append(getLastname());
 		}
 
-		
 		if (null != getFirstname()) {
 			if (0 != sb.length()) {
 				sb.append(", ");
@@ -74,18 +87,17 @@ public class PersonsDTO {
 			sb.append(getFirstname());
 		}
 
-		
 		if (null != getCwid()) {
 			if (0 != sb.length()) {
 				sb.append(" ");
 			}
-			
+
 			sb.append("(");
 			sb.append(getCwid());
 			sb.append(")");
 		}
-			
+
 		return sb.toString();
 	}
-	
+
 }
