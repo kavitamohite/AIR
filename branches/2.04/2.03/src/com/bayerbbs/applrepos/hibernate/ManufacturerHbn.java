1a09252c894d292b2c589f59256ecd9bd91d06a8
eovwj <eovwj@d2170373-ae6f-44b7-9150-2eec3990abfc>
2015-07-23 10:51:27 +0000

    10562
    git-svn-id: http://10.205.116.236/svn/AIR@851 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.04/2.03/src/com/bayerbbs/applrepos/hibernate/ManufacturerHbn.java
+++ branches/2.04/2.03/src/com/bayerbbs/applrepos/hibernate/ManufacturerHbn.java
@@ -1,5 +1,9 @@
 package com.bayerbbs.applrepos.hibernate;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,6 +15,10 @@ import org.hibernate.Transaction;
 import com.bayerbbs.applrepos.domain.Partner;
 import com.bayerbbs.applrepos.dto.KeyValueDTO;
 public class ManufacturerHbn  extends BaseHbn{
+	
+	private static final String SQL_Hardware_Product = "select * from partner where partner_id in (select partner_id from hw_kategorie3)";
+	private static final String SQL_Software_Product ="select * from partner where partner_id in (select hersteller_partnid from SW_KATEGORIE2)";
+	
 	public static Partner findById(Long id) {
 		return findById(Partner.class, id);
 	}
@@ -24,34 +32,61 @@ public class ManufacturerHbn  extends BaseHbn{
 		return listDTO;
 	}
 
-	@SuppressWarnings("unchecked")
+
 	public static KeyValueDTO[] getManufacturerById(Long partnerId) {
 
 		List<KeyValueDTO> data = new ArrayList<KeyValueDTO>();
 		Transaction tx = null;
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		try {
-			tx = session.beginTransaction();
-			List<Partner> values = session.createQuery("from Partner p where p.number is null and p.deleteTimestamp is null").list();
-			data = getDTOManufacturerList(values);
-			tx.commit();
-		} catch (RuntimeException e) {
-			if (tx != null && tx.isActive()) {
-				try {
-
-					tx.rollback();
-				} catch (HibernateException e1) {
-					System.out.println("Error rolling back transaction");
+		
+			try {
+				Connection conn = session.connection();
+				Statement stmt =conn.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL_Hardware_Product);
+				while (rs.next()) {
+					KeyValueDTO keyValueDTO = new KeyValueDTO();
+					keyValueDTO.setId(rs.getLong(1));
+					keyValueDTO.setName(rs.getString(2));
+					data.add(keyValueDTO);
 				}
-				// throw again the first exception
-				throw e;
-			}
-
+				rs.close();
+				stmt.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		Collections.sort(data);
 		return data.toArray(new KeyValueDTO[0]);
 	}
 	
+	
+	public static KeyValueDTO[] getSoftwareManufacturerById(Long partnerId) {
+
+		List<KeyValueDTO> data = new ArrayList<KeyValueDTO>();
+		Transaction tx = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+			try {
+				Connection conn = session.connection();
+				Statement stmt =conn.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL_Software_Product);
+				while (rs.next()) {
+					KeyValueDTO keyValueDTO = new KeyValueDTO();
+					keyValueDTO.setId(rs.getLong(1));
+					keyValueDTO.setName(rs.getString(2));
+					data.add(keyValueDTO);
+				}
+				rs.close();
+				stmt.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Collections.sort(data);
+		return data.toArray(new KeyValueDTO[0]);
+	}
 	public static KeyValueDTO[] getSoftwareProductById(Long partnerId) {
 		List<KeyValueDTO> data = new ArrayList<KeyValueDTO>();
 		Transaction tx = null;
