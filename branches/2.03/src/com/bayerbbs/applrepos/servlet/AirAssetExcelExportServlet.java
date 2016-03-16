139280eec45267d4ea368ff1b96c338ca5f906f0
enqmu <enqmu@d2170373-ae6f-44b7-9150-2eec3990abfc>
2016-03-16 11:47:18 +0000

    Updated for asset export/import functionality - C0000049066
    git-svn-id: http://10.205.116.236/svn/AIR@1027 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.03/src/com/bayerbbs/applrepos/servlet/AirAssetExcelExportServlet.java
+++ branches/2.03/src/com/bayerbbs/applrepos/servlet/AirAssetExcelExportServlet.java
@@ -17,8 +17,12 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.bayerbbs.applrepos.domain.HardwareComponent;
+import com.bayerbbs.applrepos.domain.Land;
+import com.bayerbbs.applrepos.domain.Standort;
 import com.bayerbbs.applrepos.dto.AssetViewDataDTO;
 import com.bayerbbs.applrepos.hibernate.HardwareComponentHbn;
+import com.bayerbbs.applrepos.hibernate.LokationItemHbn;
 import com.bayerbbs.applrepos.hibernate.SoftwareComponentHbn;
 import com.bayerbbs.applrepos.service.AssetManagementParameterInput;
 import com.bayerbbs.applrepos.service.AssetManagementParameterOutput;
@@ -29,6 +33,10 @@ public class AirAssetExcelExportServlet extends HttpServlet {
 	private static final String[] COLUMNS = { "SAP Description", "PSP Element",
 			"Cost center", "Site", "Serial Number", "Technical Master",
 			"Technical Number/Asset-Id", "Inventory Number", "Organizational Unit" };
+	private static final String[] EXPORTED_COLUMNS = { "Company Code", "Company Name", "Manufacturer", "Type", "Model", "Serial Number",
+		"Tech.Nr.", "Country", "Site", "Building", "Room", "Rack - Position", "Inventory Number", "Order-Nr.",
+		"PSP - Element", "Cost Center", "User", "DC-Name", "Anzeige ID", "IP-Adresse-RIB", "Sub-Net-Mask", "Default Gateway" };
+	
 	private static final String UNDERSCORE = "_";
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
@@ -39,16 +47,170 @@ public class AirAssetExcelExportServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 		setUpResponse(req, res);
-
-		Workbook workbook = createWorkbook(req, res);
+		
+		String selectedHwAssets = req.getParameter("selectedHwAssets");
+		System.out.println("Assets >>>>>>>>>>>>>>>> "+selectedHwAssets);
+		System.out.println("Language >>>>>>>>>>>>>>>> "+req.getParameter("language"));
+		
+		Workbook workbook = null;
+		if(selectedHwAssets != null && !selectedHwAssets.isEmpty())
+		{
+			workbook = createExportedAssetsSheet(req);
+		}
+		else {
+			workbook = createWorkbook(req, res);
+		}
 		workbook.write(res.getOutputStream());
 	}
+	
+	private Workbook createExportedAssetsSheet(HttpServletRequest req) {
+		Workbook workbook = new XSSFWorkbook();
+		
+		String selectedHwAssets = req.getParameter("selectedHwAssets");
+		
+		if(selectedHwAssets == null || selectedHwAssets.isEmpty())
+		{
+			return null;
+		}
+		
+		Sheet sheet = workbook.createSheet("Sheet1");
+		((XSSFSheet) sheet).setDefaultColumnWidth(25);
+		
+		Row headerRow = sheet.createRow(0);
+		Cell headerCell = null;
 
+		CellStyle headerRowStyle = workbook.createCellStyle();
+		headerRowStyle.setWrapText(true);
+		headerRowStyle.setAlignment(CellStyle.ALIGN_CENTER);
+		
+		for (int i = 0; i < EXPORTED_COLUMNS.length; i++) {
+			headerCell = headerRow.createCell(i);
+			headerCell.setCellValue(EXPORTED_COLUMNS[i]);
+			headerCell.setCellStyle(headerRowStyle);
+		}
+		try {
+			String[] hardwareIdArr = selectedHwAssets.split("~");
+			if(hardwareIdArr != null && hardwareIdArr.length > 0)
+			{
+				int rowCount = 1;
+				for(String hwId : hardwareIdArr)
+				{
+					try {
+						if(hwId != null && !hwId.isEmpty()) {
+							long hardwareId = Long.parseLong(hwId);
+							HardwareComponent objHardwareComponent = HardwareComponentHbn.findHardwareComponentById(hardwareId);
+							if(objHardwareComponent != null) {
+								Row row = sheet.createRow(rowCount++);
+								
+								Cell cell = null;
+								if(objHardwareComponent.getPartner() != null)
+								{
+									cell = row.createCell(0);
+									cell.setCellValue(objHardwareComponent.getPartner().getNumber());
+									
+									cell = row.createCell(1);
+									cell.setCellValue(objHardwareComponent.getPartner().getName());
+								}
+								
+
+								cell = row.createCell(2);
+								if(objHardwareComponent.getHardwareCategory3() != null && objHardwareComponent.getHardwareCategory3().getPartner() != null) {
+									cell.setCellValue(objHardwareComponent.getHardwareCategory3().getPartner().getName());
+								}
+								cell = row.createCell(3);
+								if(objHardwareComponent.getHardwareCategory3() != null) {
+									cell.setCellValue(objHardwareComponent.getHardwareCategory3().getHwKategory3());
+								}
+								cell = row.createCell(4);
+								if(objHardwareComponent.getHardwareCategory4() != null) {
+									cell.setCellValue(objHardwareComponent.getHardwareCategory4().getHwKategory4());
+								}
+								cell = row.createCell(5);
+								cell.setCellValue(objHardwareComponent.getSerialNumber());
+
+								cell = row.createCell(6);
+								cell.setCellValue(objHardwareComponent.getTechnicalNumber());
+
+								if(objHardwareComponent.getSchrank() != null)
+								{
+									cell = row.createCell(11);
+									cell.setCellValue(objHardwareComponent.getSchrank().getName());
+									if(objHardwareComponent.getSchrank().getRoom() != null)
+									{
+										cell = row.createCell(10);
+										cell.setCellValue(objHardwareComponent.getSchrank().getRoom().getName());
+										
+										if(objHardwareComponent.getSchrank().getRoom().getBuildingArea() != null && objHardwareComponent.getSchrank().getRoom().getBuildingArea().getBuilding() != null)
+										{
+											cell = row.createCell(9);
+											cell.setCellValue(objHardwareComponent.getSchrank().getRoom().getBuildingArea().getBuilding().getName());
+											
+											if(objHardwareComponent.getSchrank().getRoom().getBuildingArea().getBuilding().getTerrain() != null && objHardwareComponent.getSchrank().getRoom().getBuildingArea().getBuilding().getTerrain().getStandort() != null)
+											{
+												cell = row.createCell(8);
+												Standort standort = objHardwareComponent.getSchrank().getRoom().getBuildingArea().getBuilding().getTerrain().getStandort(); 
+												String site = "DE".equalsIgnoreCase(req.getParameter("language")) ? standort.getName() : standort.getNameEn();
+												cell.setCellValue(site);
+												
+												Long landId = standort.getLandId();
+												
+												Land land = LokationItemHbn.findLandById(landId);
+												
+												if(land != null)
+												{
+													String country = "DE".equalsIgnoreCase(req.getParameter("language")) ? land.getName() : land.getNameEn();
+													cell = row.createCell(7);
+													cell.setCellValue(country);
+												}
+											}
+											
+										}
+										
+									}
+									
+								}
+								
+								cell = row.createCell(12);
+								cell.setCellValue(objHardwareComponent.getInventoryP69());
+								
+								cell = row.createCell(13);
+								cell.setCellValue(objHardwareComponent.getBestSellText());
+								
+								cell = row.createCell(14);
+								cell.setCellValue(objHardwareComponent.getAmKommision());
+								
+								if(objHardwareComponent.getKonto() != null) {
+									cell = row.createCell(15);
+									cell.setCellValue(objHardwareComponent.getKonto().getName());
+								}
+								
+								/*if(objHardwareComponent.getItSystem() != null) {
+									cell = row.createCell(17);
+									cell.setCellValue(objHardwareComponent.getItSystem().getName());
+								}*/
+								
+							}
+						}
+					} catch(Exception ex)
+					{
+						System.out.println("Error -----> "+ex.getMessage());
+						ex.printStackTrace();
+					}
+				}
+			}
+		} catch(Exception ex)
+		{
+			System.out.println("Error -----> "+ex.getMessage());
+			ex.printStackTrace();
+		}
+		return workbook;
+	}
+	
 	private void setUpResponse(HttpServletRequest req, HttpServletResponse res) {
 		String query = req.getParameter("query");
 		String cwid = req.getParameter("cwid");
 		String searchAction = req.getParameter("searchAction");
-
+		
 		String fileName = "AirAssetExport";
 
 		if (cwid != null && cwid.length() > 0)
