8aff51ec6192f7b2b93a5de9103e264ba1ddce4f
enqmu <enqmu@d2170373-ae6f-44b7-9150-2eec3990abfc>
2016-03-16 11:45:14 +0000

    Updated for asset export/import functionality - C0000049066
    git-svn-id: http://10.205.116.236/svn/AIR@1025 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.03/src/com/bayerbbs/applrepos/hibernate/HardwareComponentHbn.java
+++ branches/2.03/src/com/bayerbbs/applrepos/hibernate/HardwareComponentHbn.java
@@ -1,14 +1,22 @@
 package com.bayerbbs.applrepos.hibernate;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.CriteriaSpecification;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
@@ -51,9 +59,8 @@ public class HardwareComponentHbn {
 					"%" + input.getQuery() + "%").ignoreCase();
 			Criterion pspElement = Restrictions.like("amKommision",
 					"%" + input.getQuery() + "%").ignoreCase();
-			criteria.createAlias("konto", "konto");
-			Criterion kontoName = Restrictions.like("konto.name",
-					"%" + input.getQuery() + "%").ignoreCase();
+			criteria.createAlias("konto", "konto", CriteriaSpecification.LEFT_JOIN);
+			Criterion kontoName = Restrictions.and(Restrictions.isNotNull("konto"), Restrictions.like("konto.name", "%" + input.getQuery() + "%").ignoreCase());
 			Criterion serialNumber = Restrictions.like("serialNumber",
 					"%" + input.getQuery() + "%").ignoreCase();
 			Criterion technicalMaster = Restrictions.like("technicalMaster",
@@ -66,9 +73,16 @@ public class HardwareComponentHbn {
 					"%" + input.getQuery() + "%").ignoreCase();
 
 			Criterion completeCondition = Restrictions.disjunction()
-					.add(hwName).add(sapDescription).add(pspElement).add(kontoName)
-					.add(serialNumber).add(technicalMaster)
-					.add(technicalNumber).add(inventoryNumber).add(orgUnit);
+					.add(hwName)
+					.add(sapDescription)
+					.add(pspElement)
+					.add(kontoName)
+					.add(serialNumber)
+					.add(technicalMaster)
+					.add(technicalNumber)
+					.add(inventoryNumber)
+					.add(orgUnit)
+					;
 			criteria.add(completeCondition);
 
 			if (input.getSort() != null) {
@@ -434,6 +448,19 @@ public class HardwareComponentHbn {
 		}
 		return null;
 	}
+	private static HardwareComponent findByIndenNumber(String indentNumber) {
+		HardwareComponent value = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			Criteria criteria = session.createCriteria(HardwareComponent.class);
+			criteria.add(Restrictions.eq("name", indentNumber));
+			value = (HardwareComponent) criteria.uniqueResult();
+			session.close();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		return value;
+	}	
 
 	private static HardwareComponent getHardwareComponent(AssetViewDataDTO dto) {
 		HardwareComponent hardwareComponent = new HardwareComponent();
@@ -457,7 +484,7 @@ public class HardwareComponentHbn {
 		} else {
 			hardwareComponent.setName(getIdentNumber());
 		}
-		if(dto.getInventoryNumber().length() == 0){
+		if(dto.getInventoryNumber() != null && dto.getInventoryNumber().length() == 0){
 			if(hardwareComponent.getInventoryStockNumber() == null){
 				hardwareComponent.setInventoryStockNumber("ExtInventory");
 				hardwareComponent.setInventoryP69(null);
@@ -519,5 +546,476 @@ public class HardwareComponentHbn {
 		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
 		return df.format(new Date(System.currentTimeMillis()));
 	}
+	
+	/**
+	 * @author enqmu
+	 * This method checks for Hardware component exists for an inventory number or not.
+	 * @param inventoryP69
+	 * @return boolean
+	 */
+	public static boolean isHardwareComponentByInventoryNumberExists(String inventoryP69) {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		boolean returnFlag = true;
+		try {
+			
+			Criteria criteria = session.createCriteria(HardwareComponent.class);
+			criteria.add(Restrictions.eq("inventoryP69", inventoryP69));
+			List<HardwareComponent> values = (List<HardwareComponent>) criteria.list();
+			if(values == null || values.isEmpty())
+			{
+				returnFlag = false;
+			}
+		} catch (Exception e) {
+			System.out.println("Error ------> "+e.getMessage());
+			e.printStackTrace();
+			returnFlag = true;
+		}finally{
+			session.close();
+		}
+		return returnFlag;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static HardwareComponent findHardwareComponentById(Long assetId) {
+		Transaction tx = null;
+		List<HardwareComponent> values = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			tx = session.beginTransaction();
+			Criteria criteria = session.createCriteria(HardwareComponent.class); 
+			criteria.setFetchMode("hardwareCategory3", FetchMode.EAGER);
+			criteria.setFetchMode("hardwareCategory4", FetchMode.EAGER); 
+			criteria.setFetchMode("schrank", FetchMode.EAGER);
+			criteria.setFetchMode("konto", FetchMode.EAGER);
+			criteria.setFetchMode("konto", FetchMode.EAGER); 
+			criteria.setFetchMode("itSystem", FetchMode.EAGER);
+			criteria.setFetchMode("partner", FetchMode.EAGER);
+			criteria.add(Restrictions.eq("id", assetId));
+			values = (List<HardwareComponent>) criteria.list();
+			tx.commit();
+			session.close();
+		} catch (RuntimeException e) {
+			if (tx != null && tx.isActive()) {
+				try {
+					tx.rollback();
+				} catch (HibernateException e1) {
+					System.out.println("Error rolling back transaction");
+				}
+				throw e;
+			}
+		}
+		if(values != null && values.size() > 0){
+			return values.get(0);
+		}
+		return null;
+	}
+	
+	public static String saveHardwareAssets(List<AssetViewDataDTO> assests)
+	{
+		String message = "File uploaded successfully.";
+		String indentNumber=null;
+		String itSystemString=null;
+		ItSystem itSystem=null;
+		AssetViewDataDTO assetDto = null;
+		List<String> indentNumbers= new ArrayList<String>();
+		 try {
+			Session session = HibernateUtil.getSession();
+			Connection conn = session.connection();
+			PreparedStatement pstmt = conn.prepareStatement("insert into HARDWAREKOMPONENTE (HW_NAME, ITSET, HERSTELLER_PARTNID, HW_KATEGORIE2_ID, HW_KATEGORIE3_ID, HW_KATEGORIE4_ID, INVENTAR_P69, SCHRANK_ID, sub_responsible, SERIEN_NR, TECHNISCHE_NR, AM_BESTELL_TEXT, AM_KOMMISSION, KONTO_ID, INSERT_USER, INSERT_QUELLE, UPDATE_TIMESTAMP, UPDATE_USER, UPDATE_QUELLE, ANFORDERER, CWID_VERANTW_BETR, OWNER_PARTNID) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			int batchSize = 20;
+			int hwCount = 0;
+			
+			for (AssetViewDataDTO asset : assests) {
+				++hwCount;
+		    indentNumber = UUID.randomUUID().toString();
+		    indentNumbers.add(indentNumber);
+			pstmt.setString(1, indentNumber);
+			Long itSet = null;
+			if(asset.getCwid() != null) {
+				itSet = new Long(AirKonstanten.IT_SET_DEFAULT);
+				String strItSet = ApplReposHbn.getItSetFromCwid(asset.getRequesterId());
 
+				if (null != strItSet) {
+					itSet = Long.parseLong(strItSet);
+				}
+			}
+			else
+			{
+				itSet = new Long(AirKonstanten.IT_SET_DEFAULT);
+			}
+			if(itSet == null)
+			{
+				pstmt.setNull(2, java.sql.Types.NULL);
+			} else {
+				pstmt.setLong(2, itSet);
+			}
+			if(asset.getManufacturerId() == null)
+			{
+				pstmt.setNull(3, java.sql.Types.NULL);
+			} else {
+				pstmt.setLong(3, asset.getManufacturerId());
+			}
+			if(asset.getSubcategoryId() == null)
+			{
+				pstmt.setNull(4, java.sql.Types.NULL);
+			} else {
+				pstmt.setLong(4, asset.getSubcategoryId());
+			}
+			if(asset.getTypeId() == null)
+			{
+				pstmt.setNull(5, java.sql.Types.NULL);
+			} else {
+				pstmt.setLong(5, asset.getTypeId());
+			}
+			if(asset.getModelId() == null)
+			{
+				pstmt.setNull(6, java.sql.Types.NULL);
+			} else {
+				pstmt.setLong(6, asset.getModelId());
+			}
+			if(asset.getInventoryNumber() == null)
+			{
+				pstmt.setNull(7, java.sql.Types.NULL);
+			} else {
+				pstmt.setString(7, asset.getInventoryNumber());
+			}
+			if(asset.getRackId() != null)
+			{
+				pstmt.setLong(8, asset.getRackId());
+			}
+			else
+			{
+				pstmt.setNull(8, java.sql.Types.NULL);
+			}
+			if(asset.getOrganizationalunit() != null)
+			{
+				pstmt.setString(9, asset.getOrganizationalunit());
+			}
+			else
+			{
+				pstmt.setNull(9, java.sql.Types.NULL);
+			}
+			if(asset.getSerialNumber() != null)
+			{
+				pstmt.setString(10, asset.getSerialNumber());
+			}
+			else
+			{
+				pstmt.setNull(10, java.sql.Types.NULL);
+			}
+			if(asset.getTechnicalNumber() != null)
+			{
+				pstmt.setString(11, asset.getTechnicalNumber());
+			}
+			else
+			{
+				pstmt.setNull(11, java.sql.Types.NULL);
+			}
+			if(asset.getOrderNumber() != null)
+			{
+				pstmt.setString(12, asset.getOrderNumber());
+			}
+			else
+			{
+				pstmt.setNull(12, java.sql.Types.NULL);
+			}
+			if(asset.getPspElement() != null)
+			{
+				pstmt.setString(13, asset.getPspElement());
+			}
+			else
+			{
+				pstmt.setNull(13, java.sql.Types.NULL);
+			}
+			if(asset.getCostCenterId() != null)
+			{
+				pstmt.setLong(14, asset.getCostCenterId());
+			}
+			else
+			{
+				pstmt.setNull(14, java.sql.Types.NULL);
+			}
+			if(asset.getCwid() != null)
+			{
+				pstmt.setString(15, asset.getCwid());
+			}
+			else
+			{
+				pstmt.setNull(15, java.sql.Types.NULL);
+			}
+			if(AirKonstanten.APPLICATION_GUI_NAME != null)
+			{
+				pstmt.setString(16, AirKonstanten.APPLICATION_GUI_NAME);
+			}
+			else
+			{
+				pstmt.setNull(16, java.sql.Types.NULL);
+			}
+			if(ApplReposTS.getCurrentTimestamp() != null)
+			{
+				pstmt.setTimestamp(17, ApplReposTS.getCurrentTimestamp());
+			}
+			else
+			{
+				pstmt.setNull(17, java.sql.Types.NULL);
+			}
+			if(asset.getCwid() != null)
+			{
+				pstmt.setString(18, asset.getCwid());
+			}
+			else
+			{
+				pstmt.setNull(18, java.sql.Types.NULL);
+			}
+			if(AirKonstanten.APPLICATION_GUI_NAME != null)
+			{
+				pstmt.setString(19, AirKonstanten.APPLICATION_GUI_NAME);
+			}
+			else
+			{
+				pstmt.setNull(19, java.sql.Types.NULL);
+			}
+			if(asset.getCwid() != null)
+			{
+				pstmt.setString(20, asset.getCwid());
+			}
+			else
+			{
+				pstmt.setNull(20, java.sql.Types.NULL);
+			}
+			if(asset.getCostCenterManagerId() != null)
+			{
+				pstmt.setString(21, asset.getCostCenterManagerId());
+			}
+			else
+			{
+				pstmt.setNull(21, java.sql.Types.NULL);
+			}
+			if(asset.getOwnerId() != null)
+			{
+				pstmt.setLong(22, asset.getOwnerId());
+			}
+			else
+			{
+				pstmt.setNull(22, java.sql.Types.NULL);
+			}
+			pstmt.addBatch();
+			
+			
+			if(hwCount % batchSize == 0)
+			{
+				int[] resultCount = pstmt.executeBatch();
+				System.out.println("Total records saved >>>>>>>> " + resultCount.length);
+				pstmt.clearBatch();
+			}   
+			
+		}
+			
+		if(hwCount % batchSize != 0) {
+			int[] resultCount = pstmt.executeBatch();
+			pstmt.clearBatch();
+			System.out.println("Remaining HW records saved >>>>>>>> " + resultCount.length);
+		}
+
+		pstmt.close();
+		conn.close();
+		session.close();
+		} catch(Exception ex)
+		{
+			System.out.println("Error ------> "+ex.getMessage());
+			ex.printStackTrace();
+			message = "File is not imported. Please contact ITILCenter Help Desk.";
+		}
+		
+		HashMap<Long, Long> hwIDIndent = new HashMap<Long, Long>();
+
+		try {
+			
+			HardwareComponent haComponent=null;
+			
+			for(int i=0; i < assests.size(); i++){
+				indentNumber = indentNumbers.get(i);
+				assetDto = assests.get(i);
+				itSystemString = assetDto.getSystemPlatformName();
+
+				if(itSystemString != null) {
+					itSystem = ItSystemHbn.findItSystemByName(itSystemString);
+					if(itSystem == null) {
+						itSystem = new ItSystem();
+					}
+					itSystem.setInsertQuelle(AirKonstanten.APPLICATION_GUI_NAME);
+					itSystem.setInsertTimestamp(ApplReposTS.getCurrentTimestamp());
+					itSystem.setInsertUser(assetDto.getCwid());
+					itSystem.setCiOwner(assetDto.getCwid());
+					itSystem.setItSystemName(assetDto.getSystemPlatformName());
+					itSystem.setUpdateQuelle(AirKonstanten.APPLICATION_GUI_NAME);
+					itSystem.setUpdateTimestamp(ApplReposTS.getCurrentTimestamp());
+					itSystem.setUpdateUser(assetDto.getCwid());
+					itSystem.setDeleteUser(null);
+					itSystem.setDeleteTimestamp(null);
+					itSystem.setDeleteUser(null);
+					itSystem.setDeleteQuelle(null);
+					ItSystemHbn.saveItSystem(itSystem);
+					
+					haComponent=findByIndenNumber(indentNumber);
+					hwIDIndent.put(haComponent.getId(), itSystem.getId());
+					
+				}
+			}
+			} catch (Exception ex) {
+				System.out.println("Error ------> "+ex.getMessage());
+				ex.printStackTrace();
+			}
+				Session session1 = HibernateUtil.getSession();
+				Connection conn1 = session1.connection();
+				try {
+					PreparedStatement pstmtITSys = conn1.prepareStatement("insert into it_system_hw (IT_SYSTEM_ID, HW_ID, INSERT_USER, INSERT_QUELLE, UPDATE_TIMESTAMP, UPDATE_QUELLE, UPDATE_USER) values(?,?,?,?,?,?,?)");
+					int batchSize = 20;
+					int itSysCount = 0;
+					
+					for(Long key : hwIDIndent.keySet())
+					{
+						++itSysCount;
+						pstmtITSys.setLong(1, hwIDIndent.get(key));
+						pstmtITSys.setLong(2, key);
+						if(assetDto.getCwid() != null)
+						{
+							pstmtITSys.setString(3, assetDto.getCwid());
+							pstmtITSys.setString(7, assetDto.getCwid());
+						}
+						else
+						{
+							pstmtITSys.setNull(3, java.sql.Types.NULL);
+							pstmtITSys.setNull(7, java.sql.Types.NULL);
+						}
+						if(AirKonstanten.APPLICATION_GUI_NAME != null)
+						{
+							pstmtITSys.setString(4, AirKonstanten.APPLICATION_GUI_NAME);
+							pstmtITSys.setString(6, AirKonstanten.APPLICATION_GUI_NAME);
+						}
+						else
+						{
+							pstmtITSys.setNull(4, java.sql.Types.NULL);
+							pstmtITSys.setNull(6, java.sql.Types.NULL);
+						}
+						if(ApplReposTS.getCurrentTimestamp() != null)
+						{
+							pstmtITSys.setTimestamp(5, ApplReposTS.getCurrentTimestamp());
+						}
+						else
+						{
+							pstmtITSys.setNull(5, java.sql.Types.NULL);
+						}
+						pstmtITSys.addBatch();
+						if (itSysCount % batchSize == 0) {
+							int[] countPstmtITSys = pstmtITSys.executeBatch();
+							System.out
+									.println("Total ITSysHwd records saved >>>>>>>> "
+											+ countPstmtITSys.length);
+							pstmtITSys.clearBatch();
+						}
+		
+					}
+					if(itSysCount % batchSize != 0){
+						int[] resultCount = pstmtITSys.executeBatch();
+						pstmtITSys.clearBatch();
+						System.out.println("Remaining IT System Hardware records saved >>>>>>>> " + resultCount.length);
+					}
+					pstmtITSys.close();
+					conn1.close();
+					session1.close();
+				}
+				catch(Exception ex){
+					System.out.println("Error ------> "+ex.getMessage());
+					ex.printStackTrace();
+					message = "Errors occured during saving DC Names. Please contact ITILCenter Help Desk.";
+				}
+		return message;
+	}
+	
+
+	
+	private static long getMaximumHardwareId()
+	{
+		long hwId = -1l;
+		try {
+			String sql = "select max(HW_ID) from HARDWAREKOMPONENTE order by HW_ID desc";
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			Connection conn = session.connection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				hwId = rs.getLong(1);
+				break;
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+			session.close();
+			System.out.println("hwId >>>>>>>>>>>>> "+hwId);
+		} catch(Exception ex)
+		{
+			System.out.println("Error ------> "+ex.getMessage());
+			ex.printStackTrace();
+		}
+		return hwId;
+	}
+	
+	private static long getMaximumItSysHwId()
+	{
+		long itSysHwId = -1l;
+		try {
+			String sql = "select max(IT_SYSTEM_HW_ID) from it_system_hw order by IT_SYSTEM_HW_ID desc";
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			Connection conn = session.connection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				itSysHwId = rs.getLong(1);
+				break;
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+			session.close();
+			System.out.println("ItSysHwId >>>>>>>>>>>>> "+itSysHwId);
+		} catch(Exception ex)
+		{
+			System.out.println("Error ------> "+ex.getMessage());
+			ex.printStackTrace();
+		}
+		return itSysHwId;
+	}
+	
+	public static boolean isPSPElementExists(String pspElement)
+	{
+		boolean returnFlag = false;
+		try {
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			Connection conn = session.connection();
+			PreparedStatement pstmt = conn.prepareStatement("select * from Konto k where k.KONTO_ART='PSP' and  k.KONTO_NAME = ?");
+			pstmt.setString(1, pspElement);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				returnFlag = true;
+				break;
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+			session.close();
+			System.out.println("returnFlag >>>>>>>>>>>>> "+returnFlag);
+		} catch(Exception ex)
+		{
+			System.out.println("Error ------> "+ex.getMessage());
+			ex.printStackTrace();
+			returnFlag = false;
+		}
+		return returnFlag;
+	}
+	
 }
