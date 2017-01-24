edde615970ab83ed11fcb01e59a88dfb33653e63
enlik <enlik@d2170373-ae6f-44b7-9150-2eec3990abfc>
2017-01-24 11:25:34 +0000

    version change
    git-svn-id: http://10.205.116.236/svn/AIR@1339 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/2.03/src/com/bayerbbs/applrepos/constants/AirKonstanten.java
+++ branches/2.03/src/com/bayerbbs/applrepos/constants/AirKonstanten.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 
 public final class AirKonstanten {
-	public final static String AIR_VERSION = "4.02";
+	public final static String AIR_VERSION = "4.03";
 	public final static String COMPRESSEDCODEDIR = "compressed";
 	
 	public final static String CACHENAME = "com.bayerbbs.applrepos.LogonData";
@@ -14,7 +14,7 @@ public final class AirKonstanten {
 	public final static String SERVERNAME_DEV = "by0nh9.de.bayer.cnb";
 	public final static String SERVERNAME_BMS_PROD = "mdelevs0300.ad.glpoly.net";
 	public final static String SERVERNAME_BMS_QA = "mdelevs0475.ad.glpoly.net";
-	public final static String TRANSBASE_PROD_HOST = "byob01.bayer-ag.com";
+	public final static String TRANSBASE_PROD_HOST = "by0ok6.de.bayer.cnb";
 	public final static String TRANSBASE_QA_HOST = "by0ot4.de.bayer.cnb";
 	public final static String TRANSBASE_BMS_PROD_HOST_SERVICENAME = "MTRANSBP";
 	public final static String TRANSBASE_BMS_QA_HOST_SERVICENAME = "MTRANSBQ";
