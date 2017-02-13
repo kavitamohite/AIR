deb9e593305a0cb66f6de58da94cd8634255e5be
enlik <enlik@d2170373-ae6f-44b7-9150-2eec3990abfc>
2017-02-13 11:10:28 +0000

    C-eye IAM change
    git-svn-id: http://10.205.116.236/svn/AIR@1346 d2170373-ae6f-44b7-9150-2eec3990abfc

--- trunk/src/com/bayerbbs/applrepos/common/LDAPAuthCeye.java
+++ trunk/src/com/bayerbbs/applrepos/common/LDAPAuthCeye.java
@@ -1,7 +1,9 @@
 package com.bayerbbs.applrepos.common;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.text.MessageFormat;
+
 import javax.naming.AuthenticationException;
 import javax.naming.Context;
 import javax.naming.NamingException;
@@ -19,12 +21,114 @@ public class LDAPAuthCeye {
 	
 	private static final String LDAP_FACTORY = "com.sun.jndi.ldap.LdapCtxFactory";
 	
-	private static final String ldapURL = "ldaps://ldaps.bayer-ag.com:636/o=bayer";	// IBM LDAP Service
-	 
+	private static final String ldapURL = "ldaps://DE.bayer.cnb:3269/";	// IBM LDAP Service
+	private static final String MASCHINE_USER = "CN=MXSNT,OU=Non-PersonalMailboxAccounts,OU=Resources,OU=_DomainOperations,DC=DE,DC=bayer,DC=cnb";
+	private static final String MASCHINE_USER_PWD = "isbb2007";
+	public static final String ROLE_CEYE_USER ="C-Eye User";
+	public static final String ROLE_CEYE_ADMIN_USER = "C-Eye-Admin User";
+	public static final String ROLE_CEYE_ADMIN_READER = "C-Eye-Admin Reader";
+	public static final String ROLE_CEYE_ADMIN_STAMMDATEN_PFLEGE = "C-Eye-Admin Stammdaten-Pflege";
+	public static final String ROLE_CEYE_ADMIN_STAMMDATEN_LESER = "C-Eye-Admin Stammdaten-Leser";
+	public static final String ROLE_REPORTING_READER = "Reporting Reader";
+	public static final String ROLE_CEYE_ADMIN_USER_MANAGEMENT = "C-Eye-Admin User-Management";
+	public final static String SERVERNAME_PROD = "BY0JZA.de.bayer.cnb";
+	public final static String SERVERNAME_QA = "BY0GMV.de.bayer.cnb";
+	public final static String SERVERNAME_DEV = "BY0N7A.DE.bayer.cnb";
 	
 	/**
 	 * @param args
 	 */
+	
+	public class RolePersonDTO {
+
+		
+		private String cwid;
+		private String roleName;
+	
+		
+		public String getCwid() {
+			return cwid;
+		}
+		public void setCwid(String cwid) {
+			this.cwid = cwid;
+		}
+		public String getRoleName() {
+			return roleName;
+		}
+		public void setRoleName(String roleName) {
+			this.roleName = roleName;
+		}
+		
+	}
+	
+	
+	public class LDAPAuthParameterOutput {
+		
+		private Long rcCode;
+		private String token;
+		private String cwid;
+
+		private String username;
+		private String lastLogon;
+		
+		private String result;
+		private String messages[];
+		
+		private ArrayList<RolePersonDTO> roles;
+		
+		
+		public Long getRcCode() {
+			return rcCode;
+		}
+		public void setRcCode(Long rcCode) {
+			this.rcCode = rcCode;
+		}
+		public String getToken() {
+			return token;
+		}
+		public void setToken(String token) {
+			this.token = token;
+		}
+		
+		public String getResult() {
+			return result;
+		}
+		public void setResult(String result) {
+			this.result = result;
+		}
+		public String[] getMessages() {
+			return messages;
+		}
+		public void setMessages(String[] messages) {
+			this.messages = messages;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getCwid() {
+			return cwid;
+		}
+		public void setCwid(String cwid) {
+			this.cwid = cwid;
+		}
+		public String getLastLogon() {
+			return lastLogon;
+		}
+		public void setLastLogon(String lastLogon) {
+			this.lastLogon = lastLogon;
+		}
+		public ArrayList<RolePersonDTO> getRoles() {
+			return roles;
+		}
+		public void setRoles(ArrayList<RolePersonDTO> roles) {
+			this.roles = roles;
+		}
+	}
+
+	
 	public static void main(String[] args) {
 
 		LDAPAuthCeye auth = new LDAPAuthCeye();
