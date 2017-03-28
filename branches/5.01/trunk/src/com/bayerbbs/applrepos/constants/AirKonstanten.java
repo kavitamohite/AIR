f91f2bd0f14d560fb7f4355517d01b22402de035
emria <emria@d2170373-ae6f-44b7-9150-2eec3990abfc>
2017-03-28 12:39:53 +0000

    Code changes for app server migration
    git-svn-id: http://10.205.116.236/svn/AIR@1391 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/5.01/trunk/src/com/bayerbbs/applrepos/constants/AirKonstanten.java
+++ branches/5.01/trunk/src/com/bayerbbs/applrepos/constants/AirKonstanten.java
@@ -9,8 +9,10 @@ public final class AirKonstanten {
 	public final static String COMPRESSEDCODEDIR = "compressed";
 	
 	public final static String CACHENAME = "com.bayerbbs.applrepos.LogonData";
-	public final static String SERVERNAME_PROD = "byzreg.bayer-ag.com";
-	public final static String SERVERNAME_QA = "byzreh.bayer-ag.com";
+	//public final static String SERVERNAME_PROD = "byzreg.bayer-ag.com";
+	public final static String SERVERNAME_PROD = "by0p9p.de.bayer.cnb";
+	//public final static String SERVERNAME_QA = "byzreh.bayer-ag.com";
+	public final static String SERVERNAME_QA = "by0p9q.de.bayer.cnb";
 	public final static String SERVERNAME_DEV = "by0nh9.de.bayer.cnb";
 	public final static String SERVERNAME_BMS_PROD = "mdelevs0300.ad.glpoly.net";
 	public final static String SERVERNAME_BMS_QA = "mdelevs0475.ad.glpoly.net";
