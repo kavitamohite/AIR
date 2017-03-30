9c831658a67235ad1e4c7c1e8414803ab378091f
mbrnj <mbrnj@d2170373-ae6f-44b7-9150-2eec3990abfc>
2017-03-30 08:03:09 +0000

    Version updated to 5.01
    git-svn-id: http://10.205.116.236/svn/AIR@1394 d2170373-ae6f-44b7-9150-2eec3990abfc

--- branches/5.01/trunk/src/com/bayerbbs/applrepos/constants/AirKonstanten.java
+++ branches/5.01/trunk/src/com/bayerbbs/applrepos/constants/AirKonstanten.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 
 public final class AirKonstanten {
-	public final static String AIR_VERSION = "5.0";
+	public final static String AIR_VERSION = "5.01";
 	public final static String COMPRESSEDCODEDIR = "compressed";
 	
 	public final static String CACHENAME = "com.bayerbbs.applrepos.LogonData";
