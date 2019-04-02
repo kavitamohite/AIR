bb0e4ab6a5a9ccd3b87f2f977356c3583ee50e07
eugxs <eugxs@d2170373-ae6f-44b7-9150-2eec3990abfc>
2019-04-02 11:18:47 +0000

    git-svn-id: http://10.205.116.236/svn/AIR@1723 d2170373-ae6f-44b7-9150-2eec3990abfc

---
+++ tags/AIR6.05.02_25032019/trunk/src/com/bayerbbs/applrepos/dto/ItSetDTO.java
@@ -0,0 +1,25 @@
+package com.bayerbbs.applrepos.dto;
+
+public class ItSetDTO {
+
+	private Long id;
+
+	private String itSetName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getItSetName() {
+		return itSetName;
+	}
+
+	public void setItSetName(String itSetName) {
+		this.itSetName = itSetName;
+	}
+	
+}
